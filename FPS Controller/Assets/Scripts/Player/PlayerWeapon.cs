68db2144a2d1d8d88551a1edd6232f1ac03ab9e4
Dima <dmitrismith5@gmail.com>
2022-02-24 18:17:45 -0600

    Switch machine gun to implement Weapon interface

--- FPS Controller/Assets/Scripts/Player/PlayerWeapon.cs
+++ FPS Controller/Assets/Scripts/Player/PlayerWeapon.cs
@@ -4,53 +4,52 @@ using UnityEngine;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    [SerializeField] GameObject[] _weapons;
-    [SerializeField] GameObject _bullet;
-    [SerializeField] GameObject _bulletSpawnPoint;
-    [SerializeField] GameObject _muzzleFlash;
-    [SerializeField] float _rateOfFire = 0.2f; //Fire rate seconds between shot
-    [SerializeField] float _bulletVelocity = 1500;
+    [SerializeField] MachineGunItem _weapon;
+    // [SerializeField] GameObject[] _weapons;
+    // [SerializeField] GameObject _bullet;
+    // [SerializeField] GameObject _bulletSpawnPoint;
+    // [SerializeField] GameObject _muzzleFlash;
+    // [SerializeField] float _rateOfFire = 0.2f; //Fire rate seconds between shot
+    // [SerializeField] float _bulletVelocity = 1500;
     // Start is called before the first frame update
 
-    private bool _isFiring = false;
-    private float _bulletLifetime = 2.0f;
+    // private bool _isFiring = false;
+    // private float _bulletLifetime = 2.0f;
 
     // Update is called once per frame
     void Update()
     {
         //Check if player is pressing fire button
         if(Input.GetMouseButtonDown(0)) {
-            _muzzleFlash.GetComponent<Renderer>().enabled = true;
-            _isFiring = true;
-            StartCoroutine(Firing());
-        } 
-
-        if(Input.GetMouseButtonUp(0)) {
-            _muzzleFlash.GetComponent<Renderer>().enabled = false;
-            _isFiring = false;
+            //If true, try to fire weapon
+            IWeapon canShoot = _weapon.GetComponent<IWeapon>();
+            if (canShoot != null) { _weapon.Fire(); }
         }
-        //If true, fire weapon
-        
-    }
 
-    IEnumerator Firing () {
-        while(_isFiring) {
-            //Rotate muzzle flash
-            _muzzleFlash.transform.eulerAngles = new Vector3(
-                _muzzleFlash.transform.eulerAngles.x,
-                _muzzleFlash.transform.eulerAngles.y,
-                Random.Range(0, 360) 
-            );
-            //Instantiate bullet
-            GameObject bullet = Instantiate(_bullet, 
-                _bulletSpawnPoint.transform.position, 
-                Quaternion.Euler(transform.eulerAngles.x + 90, transform.eulerAngles.y, transform.eulerAngles.z));
-            bullet.SendMessage("StartWithParameters", _bulletLifetime);
-            //FIre bullet in the direction
-            bullet.GetComponent<Rigidbody>().velocity = _bulletSpawnPoint.transform.forward * _bulletVelocity * Time.deltaTime;
-            yield return new WaitForSeconds(_rateOfFire);
+        if (Input.GetMouseButtonUp(0)) {
+            _weapon.StopFiring();
         }
         
     }
+
+    // IEnumerator Firing () {
+    //     while(_isFiring) {
+    //         //Rotate muzzle flash
+    //         _muzzleFlash.transform.eulerAngles = new Vector3(
+    //             _muzzleFlash.transform.eulerAngles.x,
+    //             _muzzleFlash.transform.eulerAngles.y,
+    //             Random.Range(0, 360) 
+    //         );
+    //         //Instantiate bullet
+    //         GameObject bullet = Instantiate(_bullet, 
+    //             _bulletSpawnPoint.transform.position, 
+    //             Quaternion.Euler(transform.eulerAngles.x + 90, transform.eulerAngles.y, transform.eulerAngles.z));
+    //         bullet.SendMessage("StartWithParameters", _bulletLifetime);
+    //         //FIre bullet in the direction
+    //         bullet.GetComponent<Rigidbody>().velocity = _bulletSpawnPoint.transform.forward * _bulletVelocity * Time.deltaTime;
+    //         yield return new WaitForSeconds(_rateOfFire);
+    //     }
+        
+    // }
     
 }
