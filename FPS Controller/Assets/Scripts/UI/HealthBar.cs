ec78ca3d988487a625b86a5a2a322aa130bc39c2
achyut <a953p488@ku.edu>
2022-04-03 20:00:10 -0500

    fixed health bar mechanics.simple miscalculation.

--- FPS Controller/Assets/Scripts/UI/HealthBar.cs
+++ FPS Controller/Assets/Scripts/UI/HealthBar.cs
@@ -19,7 +19,7 @@ public class HealthBar : MonoBehaviour
     {
         if(true)
         {
-            HealthFillImage.fillAmount = defaultHealth/currenthealth;
+            HealthFillImage.fillAmount = currenthealth/defaultHealth;
         }
     }
 }
