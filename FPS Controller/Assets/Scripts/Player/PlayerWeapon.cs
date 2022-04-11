4d05b9fcdf911cc47f484c4e69c0b2046068dc66
JayBeezy <jakebees@ku.edu>
2022-04-11 18:43:09 -0500

    Implemented pause menu
    - Fixed weapon controller to prevent shots from being fired during pause menu
    - Pause menu pauses game, gives options for : menu, options, resume

--- FPS Controller/Assets/Scripts/Player/PlayerWeapon.cs
+++ FPS Controller/Assets/Scripts/Player/PlayerWeapon.cs
@@ -9,17 +9,18 @@ public class PlayerWeapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Check if player is pressing fire button
-        if(Input.GetMouseButtonDown(0)) {
-            //If true, try to fire weapon
-            IWeapon canShoot = weapon.GetComponent<IWeapon>();
-            if (canShoot != null) { 
-                weapon.Fire(); 
+        if(PauseMenu.isPaused == false){
+            //Check if player is pressing fire button
+            if(Input.GetMouseButtonDown(0)) {
+                //If true, try to fire weapon
+                IWeapon canShoot = weapon.GetComponent<IWeapon>();
+                if (canShoot != null) { 
+                    weapon.Fire(); 
+                }
             }
+            if (Input.GetMouseButtonUp(0)) {
+                weapon.StopFiring();
+            }    
         }
-
-        if (Input.GetMouseButtonUp(0)) {
-            weapon.StopFiring();
-        }    
     }
 }
