40a2d1c9d3537f0081ed6728dbe90781e297a857
JayBeezy <jakebees@ku.edu>
2022-04-30 14:45:07 -0500

    Volume slider mapped to master volume controller

--- FPS Controller/Assets/Scripts/Menu/VolumeController.cs
+++ FPS Controller/Assets/Scripts/Menu/VolumeController.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
 {
+    public Slider volume_slider;
+
     [Range(0.0f, 1.0f)]
     [SerializeField] float masterVolume = 1.0f;
-    //float masterVolume = 1.0f;
 
-    void Update(){
-        AudioListener.volume = masterVolume;
+    public float slider_value;
+
+    //void Update(){
+    //    AudioListener.volume = masterVolume;
+    //}
+
+    void Start(){
+        volume_slider = GetComponent<Slider>();
+    }
+
+    public void AdjustVolume(float newVolume){
+        AudioListener.volume = newVolume;
     }
 }
