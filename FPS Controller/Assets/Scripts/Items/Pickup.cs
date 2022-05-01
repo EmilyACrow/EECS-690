c6be987b3401ef514e4eccc30806e87f810117aa
JayBeezy <jakebees@ku.edu>
2022-05-01 12:53:44 -0500

    Added pickup class *
    * not optimized fully

---
+++ FPS Controller/Assets/Scripts/Items/Pickup.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Pickup : MonoBehaviour
+{
+    public int totalNodes = 0;
+
+    public TMP_Text nodeCounter;
+
+    public GameObject node;
+
+    public bool isPicked = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       changeCounter();
+    }
+
+    public void changeCounter(){
+        if(totalNodes == 0){
+            nodeCounter.text = "Data Nodes: 0";
+        }
+        if(totalNodes == 1){
+            nodeCounter.text = "Data Nodes: 1";
+        }
+        if(totalNodes == 2){
+            nodeCounter.text = "Data Nodes: 2";
+        }
+        if(totalNodes == 3){
+            nodeCounter.text = "Data Nodes: 3";
+        }
+        if(totalNodes == 4){
+            nodeCounter.text = "Data Nodes: 4";
+        }
+        if(totalNodes == 5){
+            nodeCounter.text = "Data Nodes: 5";
+        }
+    }
+
+    void OnTriggerEnter(Collider other){
+        totalNodes = totalNodes + 1;
+        Debug.Log("Picked up Node");
+        node.transform.position = new Vector3(-10, -10, -10);
+    }
+
+}
