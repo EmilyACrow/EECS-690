ff3c466ac6a8cc5c63c062adb211d3bad817d031
JayBeezy <jakebees@ku.edu>
2022-05-03 14:05:32 -0500

    Added tutorial dialogue w/ timers

--- FPS Controller/Assets/Scripts/Tutorial/AudioOutput.cs
+++ FPS Controller/Assets/Scripts/Tutorial/AudioOutput.cs
@@ -7,12 +7,25 @@ public class AudioOutput : MonoBehaviour
     public AudioSource sound;
     private bool played = false;
 
+    public GameObject current_clip;
+    public GameObject next_clip;
+
+    public int time_to_next_clip = 5;
+
     void OnTriggerEnter(Collider other){
         var color = GetComponent<Renderer>();
         if(played == false){
             sound.Play();
             played = true;
             color.material.SetColor("_Color", Color.red);
+            StartCoroutine(timer());
+            //next_clip.SetActive(true);
+            current_clip.transform.position = new Vector3(10, 10, 10);
         }
+    }
+
+    IEnumerator timer(){
+        yield return new WaitForSeconds(time_to_next_clip);
+        next_clip.SetActive(true);
     }
 }
